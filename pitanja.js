7a53cfdc04c51636137b61b633be62562ce40fd0
ahmed53221 <a89171610@gmail.com>
2026-01-20 11:43:38 +0100

    Update pitanja.js

--- pitanja.js
+++ pitanja.js
@@ -1,5 +1,4 @@
 window.questions=[
-  [
   {
     question: "Koji nogometaš ima najviše osvojenih Zlatnih lopti?",
     options: ["Cristiano Ronaldo", "Lionel Messi", "Neymar", "Kylian Mbappé"],
@@ -24,5 +23,314 @@ window.questions=[
     question: "Koja država je osvojila najviše FIFA Svjetskih prvenstava?",
     options: ["Njemačka", "Italija", "Brazil", "Argentina"],
     answer: "Brazil"
+  },
+  {
+    question: "Koje godine je pao Berlinski zid?",
+    options: ["1987", "1989", "1991", "1993"],
+    answer: "1989"
+  },
+  {
+    question: "Koja država ima najviše stanovnika na svijetu?",
+    options: ["SAD", "Indija", "Rusija", "Brazil"],
+    answer: "Indija"
+  },
+  {
+    question: "Koji je programski jezik prvenstveno namijenjen za web preglednike?",
+    options: ["Python", "Java", "JavaScript", "C++"],
+    answer: "JavaScript"
+  },
+  {
+    question: "Koliko planeta ima Sunčev sistem?",
+    options: ["7", "8", "9", "10"],
+    answer: "8"
+  },
+  {
+    question: "Koji je najduži riječni tok koji protiče kroz Bosnu i Hercegovinu?",
+    options: ["Drina", "Bosna", "Vrbas", "Sava"],
+    answer: "Sava"
+  },
+  {
+    question: "Koji je glavni grad Japana?",
+    options: ["Osaka", "Kyoto", "Tokio", "Hirošima"],
+    answer: "Tokio"
+  },
+  {
+    question: "Koja planeta je najbliža Suncu?",
+    options: ["Venera", "Merkur", "Mars", "Zemlja"],
+    answer: "Merkur"
+  },
+  {
+    question: "Ko je napisao roman 'Na Drini ćuprija'?",
+    options: ["Mesa Selimović", "Ivo Andrić", "Branko Ćopić", "Danilo Kiš"],
+    answer: "Ivo Andrić"
+  },
+  {
+    question: "Koliko kontinenata postoji na Zemlji?",
+    options: ["5", "6", "7", "8"],
+    answer: "7"
+  },
+  {
+    question: "Koji sportaš je poznat pod nadimkom 'Air Jordan'?",
+    options: ["LeBron James", "Kobe Bryant", "Michael Jordan", "Shaquille O'Neal"],
+    answer: "Michael Jordan"
+  },
+  {
+    question: "Koji gas je najzastupljeniji u Zemljinoj atmosferi?",
+    options: ["Kiseonik", "Ugljen-dioksid", "Azot", "Vodonik"],
+    answer: "Azot"
+  },
+  {
+    question: "Koja država ima najviše stanovnika na svijetu?",
+    options: ["Indija", "SAD", "Kina", "Rusija"],
+    answer: "Indija"
+  },
+  {
+    question: "U kojem gradu se nalazi Koloseum?",
+    options: ["Atina", "Rim", "Milano", "Napulj"],
+    answer: "Rim"
+  },
+  {
+    question: "Koji je najveći okean na svijetu?",
+    options: ["Atlantski", "Indijski", "Arktički", "Tihi"],
+    answer: "Tihi"
+  },
+  {
+    question: "Koliko minuta traje jedno poluvrijeme u nogometu?",
+    options: ["40", "45", "50", "60"],
+    answer: "45"
+  },
+  {
+    question: "Koji metal ima hemijski simbol Fe?",
+    options: ["Bakar", "Aluminij", "Željezo", "Srebro"],
+    answer: "Željezo"
+  },
+  {
+    question: "Koja je najveća država na svijetu po površini?",
+    options: ["Kanada", "SAD", "Kina", "Rusija"],
+    answer: "Rusija"
+  },
+  {
+    question: "Koji instrument ima 88 tipki?",
+    options: ["Gitara", "Violina", "Klavir", "Harmonika"],
+    answer: "Klavir"
+  },
+  {
+    question: "Koja životinja je poznata kao 'kralj džungle'?",
+    options: ["Tigar", "Slon", "Lav", "Leopard"],
+    answer: "Lav"
+  },
+  {
+    question: "Koja valuta se koristi u većini zemalja Evropske unije?",
+    options: ["Dolar", "Euro", "Funta", "Franak"],
+    answer: "Euro"
+  },
+  {
+    question: "Koji kontinent ima najveću površinu?",
+    options: ["Afrika", "Azija", "Evropa", "Sjeverna Amerika"],
+    answer: "Azija"
+  },
+  {
+    question: "Koja država je izumitelj pizze?",
+    options: ["Francuska", "Italija", "Španija", "Grčka"],
+    answer: "Italija"
+  },
+  {
+    question: "Koji je simbol hemijskog elementa zlata?",
+    options: ["Au", "Ag", "Gd", "Zn"],
+    answer: "Au"
+  },
+  {
+    question: "Koji je najveći organ u ljudskom tijelu?",
+    options: ["Jetra", "Koža", "Pluća", "Bubrezi"],
+    answer: "Koža"
+  },
+  {
+    question: "Koja država je poznata kao 'Zemlja izlazećeg sunca'?",
+    options: ["Kina", "Japan", "Indija", "Tajland"],
+    answer: "Japan"
+  },
+  {
+    question: "Koji sport se igra na Wimbledonu?",
+    options: ["Nogomet", "Tenis", "Košarka", "Ragbi"],
+    answer: "Tenis"
+  },
+  {
+    question: "Koja životinja je najveća na svijetu?",
+    options: ["Plavetni kit", "Slon", "Megalodon", "Ajkula"],
+    answer: "Plavetni kit"
+  },
+  {
+    question: "Koja država je osvojila prvo FIFA Svjetsko prvenstvo?",
+    options: ["Brazil", "Njemačka", "Urugvaj", "Italija"],
+    answer: "Urugvaj"
+  },
+  {
+    question: "Koji je hemijski simbol za kisik?",
+    options: ["O", "K", "C", "N"],
+    answer: "O"
+  },
+  {
+    question: "Koji je najviši vrh svijeta?",
+    options: ["K2", "Everest", "Kilimandžaro", "Mont Blanc"],
+    answer: "Everest"
+  },
+  {
+    question: "Koja država ima oblik čizme?",
+    options: ["Italija", "Španija", "Grčka", "Portugal"],
+    answer: "Italija"
+  },
+  {
+    question: "Koja rijeka protiče kroz Pariz?",
+    options: ["Temza", "Seina", "Dunav", "Loara"],
+    answer: "Seina"
+  },
+  {
+    question: "Koji jezik se najviše govori u Južnoj Americi?",
+    options: ["Španski", "Portugalski", "Engleski", "Francuski"],
+    answer: "Španski"
+  },
+  {
+    question: "Koji grad je poznat kao 'Grad ljubavi'?",
+    options: ["London", "Pariz", "Venecija", "Prag"],
+    answer: "Pariz"
+  },
+  {
+    question: "Koji element čini najveći dio ljudskog tijela?",
+    options: ["Ugljik", "Vodonik", "Kiseonik", "Dušik"],
+    answer: "Kiseonik"
+  },
+  {
+    question: "Koji je simbol za novac u Europi?",
+    options: ["$","£","€","¥"],
+    answer: "€"
+  },
+  {
+    question: "Koja planeta je poznata kao Crvena planeta?",
+    options: ["Mars", "Venera", "Jupiter", "Merkur"],
+    answer: "Mars"
+  },
+  {
+    question: "Koja država je domaćin Olimpijskih igara 2024. godine?",
+    options: ["Francuska", "Japan", "Brazil", "SAD"],
+    answer: "Francuska"
+  },
+  {
+    question: "Koji je najveći kontinent po broju stanovnika?",
+    options: ["Afrika", "Azija", "Evropa", "Sjeverna Amerika"],
+    answer: "Azija"
+  },
+  {
+    question: "Koji instrument se koristi za mjerenje temperature?",
+    options: ["Barometar", "Termometar", "Higrometar", "Anemometar"],
+    answer: "Termometar"
+  },
+  {
+    question: "Koja država je poznata po kanalu Panamske?",
+    options: ["Panama", "Kostarika", "Kolumbija", "Meksiko"],
+    answer: "Panama"
+  },
+  {
+    question: "Koja životinja polaže najveće jaje?",
+    options: ["Noj", "Kokoška", "Emu", "Pingvin"],
+    answer: "Noj"
+  },
+  {
+    question: "Koji je glavni grad Australije?",
+    options: ["Sidnej", "Melburn", "Canberra", "Brisbane"],
+    answer: "Canberra"
+  },
+  {
+    question: "Koja rijeka protiče kroz Egipat?",
+    options: ["Nil", "Niger", "Senegal", "Zambezi"],
+    answer: "Nil"
+  },
+  {
+    question: "Koja država je poznata po festivalu Rio de Janeiro?",
+    options: ["Argentina", "Brazil", "Kolumbija", "Meksiko"],
+    answer: "Brazil"
+  },
+  {
+    question: "Koji je hemijski simbol za srebro?",
+    options: ["Ag", "Au", "Si", "Pb"],
+    answer: "Ag"
+  },
+  {
+    question: "Koja planina je najviša u Evropi?",
+    options: ["Elbrus", "Mont Blanc", "Matterhorn", "Grossglockner"],
+    answer: "Elbrus"
+  },
+  {
+    question: "Koja država ima najviše vulkana?",
+    options: ["Japan", "Indonezija", "Island", "Italija"],
+    answer: "Indonezija"
+  },
+  {
+    question: "Koja ptica ne može letjeti?",
+    options: ["Pingvin", "Orao", "Soko", "Golub"],
+    answer: "Pingvin"
+  },
+  {
+    question: "Koja država se smatra kolevkom demokratije?",
+    options: ["Francuska", "Grčka", "Italija", "Švedska"],
+    answer: "Grčka"
+  },
+  {
+    question: "Koja je najduža planinska lanac na svijetu?",
+    options: ["Ande", "Himalaji", "Alpi", "Kavkaz"],
+    answer: "Ande"
+  },
+  {
+    question: "Koji je najveći kontinent po površini?",
+    options: ["Afrika", "Azija", "Sjeverna Amerika", "Evropa"],
+    answer: "Azija"
+  },
+  {
+    question: "Koji grad je poznat po Big Benu?",
+    options: ["Pariz", "London", "Berlin", "Madrid"],
+    answer: "London"
+  },
+  {
+    question: "Koja država je poznata po piramidama?",
+    options: ["Egipat", "Meksiko", "Peru", "Grčka"],
+    answer: "Egipat"
+  },
+  {
+    question: "Koja hemijska supstanca daje listovima zelenu boju?",
+    options: ["Hlorofil", "Karoten", "Melanin", "Hematitin"],
+    answer: "Hlorofil"
+  },
+  {
+    question: "Koji grad je domaćin Opera House?",
+    options: ["Sidnej", "Melburn", "Auckland", "Brisbane"],
+    answer: "Sidnej"
+  },
+  {
+    question: "Koja država ima oblik trokuta na mapi?",
+    options: ["Egipat", "Italija", "Grčka", "Švedska"],
+    answer: "Egipat"
+  },
+  {
+    question: "Koja planeta ima najviše mjeseca?",
+    options: ["Saturn", "Jupiter", "Uran", "Neptun"],
+    answer: "Saturn"
+  },
+  {
+    question: "Koji jezik je službeni u Brazilu?",
+    options: ["Španski", "Portugalski", "Engleski", "Francuski"],
+    answer: "Portugalski"
+  },
+  {
+    question: "Koji element je osnovni sastojak stakla?",
+    options: ["Silicijum", "Aluminijum", "Željezo", "Bakar"],
+    answer: "Silicijum"
   }
 ]
+
+
+
+
+
+  
+
+
+
