4c4ef8ee596c6fef5e876cf8feafb8a7e655335e
adriankristicc <159646795+adriankristicc@users.noreply.github.com>
2026-01-20 10:50:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+let time = 30;
+let timerInterval;
+const timerEl = document.getElementById("timer");
+
+function startTimer() {
+  time = 30;
+  timerEl.textContent = "⏱ " + time;
+
+  timerInterval = setInterval(() => {
+    time--;
+    timerEl.textContent = "⏱ " + time;
+
+    if (time === 0) {
+      clearInterval(timerInterval);
+      nextQuestion();
+    }
+  }, 1000);
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  startTimer();
+}
