44c0659a4c0ca361fc6db9ccfd59e8b7eeed1d83
ahmed53221 <a89171610@gmail.com>
2026-01-20 11:29:53 +0100

    Create pitanja.js

---
+++ pitanja.js
@@ -0,0 +1,28 @@
+window.questions=[
+  [
+  {
+    question: "Koji nogometaš ima najviše osvojenih Zlatnih lopti?",
+    options: ["Cristiano Ronaldo", "Lionel Messi", "Neymar", "Kylian Mbappé"],
+    answer: "Lionel Messi"
+  },
+  {
+    question: "Koja država je bila domaćin prve moderne Olimpijade 1896. godine?",
+    options: ["Francuska", "Grčka", "Velika Britanija", "Italija"],
+    answer: "Grčka"
+  },
+  {
+    question: "Koja rijeka je najduža na svijetu?",
+    options: ["Nil", "Amazona", "Misisipi", "Jenisej"],
+    answer: "Amazona"
+  },
+  {
+    question: "Koja je hemijska formula za vodu?",
+    options: ["H2O", "CO2", "O2", "H2"],
+    answer: "H2O"
+  },
+  {
+    question: "Koja država je osvojila najviše FIFA Svjetskih prvenstava?",
+    options: ["Njemačka", "Italija", "Brazil", "Argentina"],
+    answer: "Brazil"
+  }
+]
